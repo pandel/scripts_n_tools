a99f98f5d52669ab45859065e1996c865c9cf9c3
Holger Pandel <holger.pandel@googlemail.com>
2018-04-23 14:00:29 +0200

    Kosmetik

--- winexe_1_1_compile/compile.sh
+++ winexe_1_1_compile/compile.sh
@@ -5,7 +5,7 @@ WINEXE_BRANCH="master"
 WINEXE_DIR="$PWD/winexe-waf"
 WINEXE_HASH="ce3588cabed68c9d83a74c9e8e7b5c129881d7b6"
 
-SAMBA_REPOSITORY="git://git.samba.org/samba.git"
+SAMBA_REPOSITORY="https://git.samba.org/samba.git"
 SAMBA_HASH="f3b650fc75b8edb27a852b88f469e8cd4a317f99"
 SAMBA_DIR="$PWD/samba"
 
@@ -16,11 +16,15 @@ SAMBA_DIR="$PWD/samba"
 apt-get -y install gcc-mingw-w64 comerr-dev libpopt-dev libbsd-dev zlib1g-dev libc6-dev python-dev libacl1-dev libldap2-dev libgnutls-dev libpam0g-dev git
 
 # get samba and winexe
-mkdir -p ${SAMBA_DIR}
-git clone ${SAMBA_REPOSITORY} ${SAMBA_DIR}
-mkdir -p ${WINEXE_DIR}
-git clone ${WINEXE_REPOSITORY} ${WINEXE_DIR}
+if [ ! -d ${SAMBA_DIR} ]; then
+  mkdir -p ${SAMBA_DIR}
+  git clone ${SAMBA_REPOSITORY} ${SAMBA_DIR} || exit 1
+fi
 
+if [ ! -d ${WINEXE_DIR} ]; then
+  mkdir -p ${WINEXE_DIR}
+  git clone ${WINEXE_REPOSITORY} ${WINEXE_DIR} || exit 1
+fi
 
 # patch samba
 cd ${SAMBA_DIR}
